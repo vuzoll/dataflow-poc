8bd3fd72ff325fb1b831cd7d565ad46367770d86
Yaroslav Yermilov <yermilov17@gmail.com>
2016-10-10 01:03:51 +0300

    #2. adapt python scripts to work with spring xd by vlad.grytsun

--- scripts/fetch-vk-data.py
+++ scripts/fetch-vk-data.py
@@ -1,8 +1,8 @@
 import vk
 import json
-import threading
 import argparse
 import time
+import sys
 
 
 REQUEST_FIELDS = ['country', 'city', 'universities', 'education', 'occupation', 'career']
@@ -10,13 +10,10 @@ START_NODE = 11582866 # me
 FIELDS_TO_REMOVE = ['uid', 'first_name', 'last_name', 'hidden']
 NECESSARY_FIELDS = ['career', 'universities', 'education', 'occupation',
                     'university_name', 'university', 'faculty_name', 'faculty']
-OUTPUT_FILE = 'UNIVERSITY_DATA1'
 EXECUTION_STATE_FILE = 'EXECUTION_STATE1'
 DEPTH = 2
 TIMER_SEC = None
 
-should_continue_execution = True
-execution_stopped = False
 queue = []
 
 api = vk.API(vk.Session())
@@ -64,7 +61,7 @@ def trim_user_data(user_data):
 
 
 def process_all_friends_data(node_id):
-    global processed_friend_ids, REQUEST_FIELDS, OUTPUT_FILE
+    global processed_friend_ids, REQUEST_FIELDS
     friends = fetch_all_friends_data(node_id, REQUEST_FIELDS)
     friends = [item for item in friends if item['user_id'] not in processed_friend_ids]
     ids = [item['user_id'] for item in friends]
@@ -72,34 +69,30 @@ def process_all_friends_data(node_id):
         processed_friend_ids.add(cur_id)
     friends = purge_user_data(friends)
     friends = trim_user_data(friends)
-    university_data_file = open(OUTPUT_FILE, 'a')
     for item in friends:
-        university_data_file.write(json.dumps(item))
-        university_data_file.write('\n')
-    university_data_file.close()
+        print json.dumps(item), "\r"
 
     return ids
 
 
 def crawl_graph(start_node_id, depth, process_data_and_get_ids_fn):
-    global queue, should_continue_execution, execution_stopped, TIMER_SEC
+    global queue, TIMER_SEC
 
     start_time = time.time()
 
     if len(queue) == 0:
         queue = [(start_node_id, 0)]
-    while len(queue) > 0 and should_continue_execution:
+    while len(queue) > 0:
         node_id, cur_depth = queue[0]
         if cur_depth >= depth:
             return
-        print 'started processing of', node_id, 'on depth', cur_depth
+        print >> sys.stderr, 'started processing of', node_id, 'on depth', cur_depth
         queue = queue[1:]
         new_node_ids = process_data_and_get_ids_fn(node_id)
-        print 'new ids count:', len(new_node_ids), 'total ids count:', len(processed_friend_ids)
+        print >> sys.stderr, 'new ids count:', len(new_node_ids), 'total ids count:', len(processed_friend_ids)
         queue += zip(new_node_ids, [cur_depth + 1 for _ in range(len(new_node_ids))])
         if TIMER_SEC is not None and time.time() - start_time > TIMER_SEC:
-            should_continue_execution = False
-    execution_stopped = True
+            break
 
 
 def persist_execution_state():
@@ -112,60 +105,36 @@ def persist_execution_state():
 
 def load_execution_state():
     global processed_friend_ids, queue
-    execution_state_file = open(EXECUTION_STATE_FILE, 'r')
-    execution_state = json.load(execution_state_file)
-    processed_friend_ids, queue = execution_state
-    processed_friend_ids = set(processed_friend_ids)
-    execution_state_file.close()
+    try:
+        execution_state_file = open(EXECUTION_STATE_FILE, 'r')
+        execution_state = json.load(execution_state_file)
+        processed_friend_ids, queue = execution_state
+        processed_friend_ids = set(processed_friend_ids)
+        execution_state_file.close()
+    except IOError:
+        print >> sys.stderr, 'execution state does not exist'
 
-
-def wait_for_user_input():
-    global should_continue_execution, execution_stopped
-    while not execution_stopped:
-        input_line = raw_input()
-        if input_line.lower() == 'stop':
-            stop_execution()
-
-
-def stop_execution():
-    global should_continue_execution, execution_stopped
-    print 'waiting for script to stop...'
-    should_continue_execution = False
-    while not execution_stopped:
-        pass
-    print 'persisting execution state...'
-    persist_execution_state()
-    print 'done'
 
 parser = argparse.ArgumentParser('Fetching university data from vk')
-parser.add_argument('--continue', help='continue execution using previously persisted state')
 parser.add_argument('--depth', help='depth of the parsing')
 parser.add_argument('--node', help='start node id')
-parser.add_argument('--file', help='output file')
 parser.add_argument('--time', help='how long script should be executed in sec')
 args = vars(parser.parse_args())
 
-should_load_execution_state = False
-if args['continue'] is not None:
-    should_load_execution_state = True
 if args['depth'] is not None:
     DEPTH = int(args['depth'])
 if args['node'] is not None:
     START_NODE = int(args['node'])
-if args['file'] is not None:
-    OUTPUT_FILE = str(args['file'])
 if args['time'] is not None:
     TIMER_SEC = int(args['time'])
 
-t = threading.Thread(target=wait_for_user_input)
-t.setDaemon(True)
-t.start()
-
-try:
-    if should_load_execution_state:
+while True:
+    try:
+        print >> sys.stderr, 'waiting for timer param in stdin'
+        TIMER_SEC = int(raw_input())
         load_execution_state()
-    crawl_graph(START_NODE, DEPTH, process_all_friends_data)
-except Exception:
-    persist_execution_state()
-else:
-    persist_execution_state()
+        crawl_graph(START_NODE, DEPTH, process_all_friends_data)
+    except Exception:
+        persist_execution_state()
+    else:
+        persist_execution_state()
