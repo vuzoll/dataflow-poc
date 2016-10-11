048f94214f99114effb5bc8dc9bf66153d5cc7e8
Yaroslav Yermilov <yermilov17@gmail.com>
2016-10-12 00:48:33 +0300

    #10. adapt script to spring xd

--- scripts/exploration-report.py
+++ scripts/exploration-report.py
@@ -1,6 +1,10 @@
 import pandas
 import json
 
+def eod():
+    sys.stdout.write("\r\n")
+    sys.stdout.flush()
+
 parser = argparse.ArgumentParser('Create exploration report based on fetched vk data')
 parser.add_argument('--input', help='input file with vk data')
 parser.add_argument('--output', help='output file with exploration report')
@@ -11,9 +15,18 @@ if args['input'] is not None:
 if args['output'] is not None:
     OUTPUT_FILE = str(args['output'])
 
-vk_data_pd = pandas.DataFrame([json.loads(line) for line in open(INPUT_FILE, 'r')])
-vk_jobs_pd = vk_data_pd.career.apply(lambda jobs: [job['position'].lower() for job in jobs if 'position' in job])
-vk_jobs_pd = pandas.Series([job for jobs in vk_jobs_pd.values for job in jobs])
-vk_jobs_set = vk_jobs_pd.value_counts().keys()
+while True:
+try:
+    print >> sys.stderr, 'waiting for request'
+    DUMP_INPUT = raw_input()
 
-vk_jobs_pd.to_json(OUTPUT_FILE)
+    vk_data_pd = pandas.DataFrame([json.loads(line) for line in open(INPUT_FILE, 'r')])
+    vk_jobs_pd = vk_data_pd.career.apply(lambda jobs: [job['position'].lower() for job in jobs if 'position' in job])
+    vk_jobs_pd = pandas.Series([job for jobs in vk_jobs_pd.values for job in jobs])
+    vk_jobs_set = vk_jobs_pd.value_counts().keys()
+
+    vk_jobs_pd.to_json(OUTPUT_FILE)
+except Exception:
+    eod()
+else:
+    eod()
