c2b6791f0c8163afa7e3254eac3204f4fd748225
Yaroslav Yermilov <yermilov17@gmail.com>
2016-10-12 00:53:23 +0300

    #10. various fixes

--- scripts/exploration-report.py
+++ scripts/exploration-report.py
@@ -1,9 +1,16 @@
 import pandas
 import json
+import sys
+import argparse
+
+INPUT_FILE = '/root/data/vk.data'
+OUTPUT_FILE = '/root/data/exploration-report.txt'
+
 
 def eod():
     sys.stdout.write("\r\n")
     sys.stdout.flush()
+
 
 parser = argparse.ArgumentParser('Create exploration report based on fetched vk data')
 parser.add_argument('--input', help='input file with vk data')
@@ -16,17 +23,17 @@ if args['output'] is not None:
     OUTPUT_FILE = str(args['output'])
 
 while True:
-try:
-    print >> sys.stderr, 'waiting for request'
-    DUMP_INPUT = raw_input()
+    try:
+        print >> sys.stderr, 'waiting for request'
+        DUMP_INPUT = raw_input()
 
-    vk_data_pd = pandas.DataFrame([json.loads(line) for line in open(INPUT_FILE, 'r')])
-    vk_jobs_pd = vk_data_pd.career.apply(lambda jobs: [job['position'].lower() for job in jobs if 'position' in job])
-    vk_jobs_pd = pandas.Series([job for jobs in vk_jobs_pd.values for job in jobs])
-    vk_jobs_set = vk_jobs_pd.value_counts().keys()
+        vk_data_pd = pandas.DataFrame([json.loads(line) for line in open(INPUT_FILE, 'r')])
+        vk_jobs_pd = vk_data_pd.career.apply(lambda jobs: [job['position'].lower() for job in jobs if 'position' in job])
+        vk_jobs_pd = pandas.Series([job for jobs in vk_jobs_pd.values for job in jobs])
+        vk_jobs_set = vk_jobs_pd.value_counts().keys()
 
-    vk_jobs_pd.to_json(OUTPUT_FILE)
-except Exception:
-    eod()
-else:
-    eod()
+        vk_jobs_pd.to_json(OUTPUT_FILE)
+    except Exception:
+        eod()
+    else:
+        eod()
